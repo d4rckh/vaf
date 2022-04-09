498fcf82db6d2ce4f4ed80bcdfe08f834f3e5b36
d4rckh <35298550+d4rckh@users.noreply.github.com>
2022-04-09 20:23:14 +0300

    create the file stream

--- src/utils/VafWordlist.nim
+++ src/utils/VafWordlist.nim
@@ -1,4 +1,27 @@
+import std/streams
+import os
+import strformat
+import strutils
+import uri
+
 import VafFuzzArguments
 
 proc prepareWordlist*(fuzzArguments: VafFuzzArguments) =
-    echo fuzzArguments
+    let wordlistFile = fuzzArguments.wordlistFile
+    let prefixes = fuzzArguments.prefixes
+    let suffixes = fuzzArguments.suffixes
+    let urlencode = fuzzArguments.urlencode
+    
+    var strm = newFileStream(wordlistFile, fmRead)
+    var line = ""
+
+    if not isNil(strm):
+        while strm.readLine(line):
+            for prefix in prefixes:
+                for suffix in suffixes:
+                    var word = prefix & line & suffix
+                    if urlencode:
+                        word = encodeUrl(word, true)
+                    echo word
+
+    strm.close()
