14c34ca084eb10cc18180e1202e265865cded779
d4rckh <35298550+d4rckh@users.noreply.github.com>
2022-04-17 23:08:28 +0300

    made everything more colorful

--- src/utils/VafLogger.nim
+++ src/utils/VafLogger.nim
@@ -2,49 +2,75 @@ import strformat
 import uri
 import strutils
 import httpclient
+import terminal
 
 import VafColors
 import ../types/[VafFuzzResult, VafFuzzArguments]
 
 proc log*(logType: string, logMessage: string): void = 
     if logType == "info":
-        echo &"{BLUEY}INFO: {RESETCOLS}{logMessage}{RESETCOLS}"
+        stdout.styledWriteLine(
+            fgBlue, "[INFO] ", logMessage, fgWhite
+        )
     if logType == "result":
-        echo &"{BLUEY}RESULT: {RESETCOLS}{logMessage}{RESETCOLS}"
+        stdout.styledWriteLine(
+            fgGreen, "[RESULT] ", logMessage, fgWhite
+        )
     if logType == "warn":
-        echo &"{ORANGE}WARN: {RESETCOLS}{logMessage}{RESETCOLS}"
+        stdout.styledWriteLine(
+            fgYellow, "[WARN] ", logMessage, fgWhite
+        )
     if logType == "header":
         return
         # disable headers, they look ugly
         # echo &"{BLUEY}\t{logMessage}{RESETCOLS}\n"
     if logType == "error":
-        echo &"{ORANGE}ERROR: {logMessage}{RESETCOLS}"
+        stdout.styledWriteLine(
+            fgRed, "[ERROR] ", logMessage, fgWhite
+        )    
     if logType == "debug":
-        echo &"{BLUEY}DEBUG: {logMessage}{RESETCOLS}"
+        stdout.styledWriteLine(
+            fgBlue, "[DEBUG] ", logMessage, fgWhite
+        )
+
 
 proc log*(logType: string, logMessage: string, logArgument: string): void = 
     if logType == "option":
-        echo &"{ORANGE}{logMessage}{RESETCOLS}: {logArgument}{RESETCOLS}"
+        stdout.styledWriteLine(
+            fgCyan, logMessage, fgWhite, ": ", logArgument, fgWhite
+        )
 
 proc printResponse*(fuzzResult: FuzzResult, fuzzArguments: FuzzArguments, threadId: int): void = 
     var urlDecoded = "" 
     var urlDisplay = ""
-    var statusColor = KHAKI
+    var statusColor = fgYellow
     var statusCode = fuzzResult.statusCode.split(" ")[0]
     if fuzzResult.urlencoded:
         urlDecoded = &"({decodeUrl(fuzzResult.word)})"
     if fuzzResult.printUrl:
         urlDisplay = fuzzResult.url
-        urlDisplay = urlDisplay.replace(fuzzResult.word, &"{RESETCOLS}{KHAKI}{fuzzResult.word}{RESETCOLS}{ORANGE}")
+        urlDisplay = urlDisplay.replace(fuzzResult.word, &"{fgWhite}{fgYellow}{fuzzResult.word}{fgWhite}{fgYellow}")
     if parseInt(statusCode) in {200 .. 299}:
-        statusColor = LIGHTGREEN
+        statusColor = fgGreen
     # its khaki by default
     # if parseInt(statusCode) in {300 .. 399}:
     #     statusColor = KHAKI
     if parseInt(statusCode) in {400 .. 499}:
-        statusColor = RED
+        statusColor = fgRed
     
-    log("result", &"{RESETCOLS}{statusColor}[{fuzzResult.statusCode}] ({fuzzResult.response.responseLength} chars) {fuzzResult.response.responseTime}ms {fuzzResult.word} {ORANGE}{urlDecoded} {urlDisplay} {RESETCOLS}")    
+    stdout.styledWriteLine(
+        # Status
+        statusColor, &"[{fuzzResult.statusCode}] ",
+        # Length
+        &"({fuzzResult.response.responseLength}c) ",
+        # Time,
+        &"{fuzzResult.response.responseTime}ms ",
+        # Word,
+        &"{fuzzResult.word} {urlDecoded} {urlDisplay}",
+        fgWhite
+    )
+
+    # log("result", &"{RESETCOLS}{statusColor}[{fuzzResult.statusCode}] ({fuzzResult.response.responseLength} chars) {fuzzResult.response.responseTime}ms {fuzzResult.word} {ORANGE}{urlDecoded} {urlDisplay} {RESETCOLS}")    
     if fuzzArguments.printheaders:
         for key, val in fuzzResult.response.headers:
-            echo &"| {ORANGE}{key}{RESETCOLS}: {val}"
+            stdout.styledWriteLine("| ", fgMagenta, key, fgWhite, ": ", val)
