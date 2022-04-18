1ae45d68605b0487235f37fb20296428d26662ab
d4rckh <35298550+d4rckh@users.noreply.github.com>
2022-04-18 14:45:09 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,5 +11,6 @@ echo "Deleting previous installation of var (if it exists)"
 rm /usr/bin/vaf
 echo "Moving vaf to /usr/bin/vaf"
 mv Linux-vaf /usr/bin/vaf
+chmod +x /usr/bin/vaf
 echo
 echo "vaf installed successfully, you can now run 'vaf'"
